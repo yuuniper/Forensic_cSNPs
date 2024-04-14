1789541ae87468937f2fa56fe823c7b4c4e18bb9
Alice Yu <alice@Alices-MacBook-Air.local>
2024-04-14 17:00:43 -0400

    update preprocessing

--- preprocessing.sh
+++ preprocessing.sh
@@ -3,7 +3,7 @@
 # Substitute the command to run your code below:
 # Input: 
 # [1] /u/INPUT_DIRECTORY (do not include FILENAME) 
-# [2] FILENAME (do not include _R1 or _R2, use ONLY sample name)
+# [2] SAMPLE_NAME (do not include _R1 or _R2, use ONLY sample name)
 # [3] /u/OUTPUT_DIRECTORY
 # [4] Enter 1 if single read, 2 if paired read
 
@@ -11,15 +11,31 @@
 # ex: srun preprocessing.sh /home/user/preprocessing_input/ sample_name /home/user/preprocessing_output/ 2
 
 
-# Create processing directory
 mkdir $3$2"_processing"
+echo "___________"$2" sample folder made___________"
 
-# HISAT2
 
-hisat2 -p 8 --dta -x $1"hg38/hg38"/genome -1 /home/nisreen/NewRNAseq/trimmomatic/$2"_R1".trim.fastq -2 /home/nisreen/NewRNAseq/trimmomatic/$2"_R2".trim.fastq -S $3$2"_processing/"$2.sam 2> $3$2"_processing/"$2.summary
+if [ "$4" -eq "2" ]; then
+    ~/utils/fastp -i /home/al018041/utils/sratoolkit.3.0.7-ubuntu64/bin/$2_1.fastq.gz -I /home/al018041/utils/sratoolkit.3.0.7-ubuntu64/bin/$2_2.fastq.gz -o $3$2"_processing"/$2_1.trim.fastq -O $3$2"_processing"/$2_2.trim.fastq
 
-echo "___________HISAT2___________"
+    echo "___________fastp___________"
 
+    hisat2 -p 8 --dta -x $1"hg38/hisat2index/hg38/genome" -1 $3$2"_processing/"$2"_1".trim.fastq -2 $3$2"_processing/"$2"_2".trim.fastq  -S $3$2"_processing/"$2.sam 2> $3$2"_processing/"$2.summary
+
+    echo "___________HISAT2___________"
+
+elif [ "$4" -eq "1" ]; then
+    ~/utils/fastp -i /home/al018041/utils/sratoolkit.3.0.7-ubuntu64/bin/"$2".fastq.gz -o "$3""$2"_processing/"$2.trim.fastq"
+
+    echo "___________fastp___________"
+
+    hisat2 --phred33 --dta -x "$1"hg38/hisat2index/hg38/genome -U "$3""$2"_processing/"$2.trim.fastq" -S "$3""$2"_processing/"$2.sam" 2> "$3""$2"_processing/"$2.summary"
+
+else
+    echo "Invalid argument: 4th argument should be either '1' or '2'"
+    echo "[1] should be for single reads."
+    echo "[2] should be for paired reads."
+fi
 
 # Convert SAM file to BAM file 
 module purge 
@@ -28,7 +44,6 @@ samtools view -bS $3$2"_processing/"$2.sam > $3$2"_processing/"$2.bam
 
 echo "___________SAMtoBAM___________"
 
-# Check the SAM file  
 module purge
 module load java/17
 module load picard3
@@ -39,7 +54,9 @@ MODE=SUMMARY
 
 echo "___________ValidateSamFile___________"
 
+
 # Mark Duplicates 
+# Load Modules
 module purge
 module load java/8
 module /share/apps/gatk-4.0.2.1
@@ -50,14 +67,10 @@ module /share/apps/gatk-4.0.2.1
 -M $3$2"_processing/"$2"_MarkedDuplicates_Metrics".txt
 
 echo "___________MarkDuplicates___________"
-
-# Add Read Groups if needed
-# If not needed, comment out. (You will most likely need this.) 
 module purge
 module load java/17
 module load picard3
 
-# Modify as needed, as these are dummy values 
 picard  AddOrReplaceReadGroups \
 I=$3$2"_processing/"$2"_MarkDuplicatesPicard".bam \
 O=$3$2"_processing/"$2"_AddReplaceRG".bam \
@@ -70,7 +83,8 @@ RGSM=$2
 echo "___________AddOrReplaceReadGroups___________"
 
 
-# Sort and Index Files 
+#samtools sort $3$2"_processing/"$2"_MarkDuplicatesPicard".bam > $3$2"_processing/"$2"_sorted".bam
+
 samtools sort $3$2"_processing/"$2"_AddReplaceRG".bam > $3$2"_processing/"$2"_sorted".bam
 samtools index $3$2"_processing/"$2"_sorted".bam
 
@@ -90,26 +104,26 @@ echo "___________Mutect2___________"
 
 # Generate SNP and Indel Files 
 
+# Load Modules
 module purge
 module load java/8
 module load /share/apps/gatk-4.0.2.1
 
+# 2/8/24 does not need modding outside gatk step and removing INDEL version
+# This step does not take a lot of time (Less than 1 minute)
 /share/apps/gatk-4.0.2.1/gatk SelectVariants \
 -R $1/hg38/hg38.fa \
 -V $3$2"_processing/"$2"_Mutect2".vcf.gz \
 --select-type-to-include SNP \
 -O $3$2"_processing/"$2"_raw_snps".vcf.gz
 
-/share/apps/gatk-4.0.2.1/gatk SelectVariants \
--R $1"hg38/hg38".fa \
--V $3$2"_processing/"$2"_Mutect2".vcf.gz \
---select-type-to-include INDEL \
--O $3$2"_processing/"$2"_raw_indels".vcf.gz
 
 echo "___________SelectVariants___________"
 
 # Filter SNP Variants 
+# These are the standard values
 
+# Load Modules
 module purge
 module load java/8
 module load /share/apps/gatk-4.0.2.1
@@ -127,7 +141,7 @@ module load /share/apps/gatk-4.0.2.1
 
 echo "___________Variant Filtration ___________"
 
-# Annotate with bcftools 
+
 module purge
 module load samtools 
 
@@ -135,8 +149,6 @@ bcftools annotate -a $1"hg38/hg38".knownGeneAddGene_name.bed.gz -c CHROM,-,INFO/
 
 echo "___________bcftools ___________"
 
-# Identify cSNPs using python script
-python find_cSNP.py $3$2"_processing/"$2"_annotatedSNPsfinalbcf.vcf $3$2"_processing/"$2"_cSNPs.txt
+python find_cSNP.py $2"_processing/"$2"_annotatedSNPsfinalbcf.vcf" $2"_processing/"$2"_cSNPs.txt"
 
 echo "___________python_cSNP ___________"
-
